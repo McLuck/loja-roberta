ece53798838bbc3b9621a000351eaed65e7cfe81
RobertaAb <robertaabreu77@gmail.com>
2021-12-08 20:03:44 -0300

    formatado o template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,9 @@
 <template>
- <div>
-   <!-- https://vuetifyjs.com/en/components/app-bars/#fade-image-on-scroll -->
-  <v-head>
-  <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="primary"
-      dark
-      inverted-scroll
-      scroll-target="#scrolling-techniques-8"
-    >
+  <div>
+    <v-app-bar color="deep-purple accent-4" dense dark>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>Page title</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -20,61 +11,33 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-8"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1500px;">
-      </v-container>
-    </v-sheet>
-  </v-card>
-  </v-head>
-
-   <v-main>
-    <!-- https://medium.com/horadecodar/como-fazer-um-tri%C3%A2ngulo-com-css-8621d57f4d35-->
-
-  <div id="triangulo-para-baixo"></div>
-
-  </v-main>
-
-  <v-footer>
-    <!-- https://vuetifyjs.com/en/components/data-iterators/#filter -->
-
-<template v-slot:footer>
-        <v-toolbar
-          class="mt-2"
-          color="indigo"
-          dark
-          flat>
-          <v-toolbar-title class="subheading">
-            This is a footer
-          </v-toolbar-title>
-        </v-toolbar>
-  </v-footer>
- </div>
+    <v-main>
+      conteudo
+    </v-main>
+    <v-footer v:bind="fixed" padless>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-footer>
+  </div>
 </template>
 
 <script>
-
 </script>
 
 <style>
-/* css triangulo*/
-div {
-    display: inline-block;
-    margin-right: 50px;
-}
-
-#triangulo-para-baixo {
-  width: 0; 
-  height: 0; 
-  border-left: 25px solid transparent;
-  border-right: 25px solid transparent;
-  border-top: 25px solid #949396;
-}
 </style>
--- src/components/OnlinePersons.vue
+++ src/components/OnlinePersons.vue
@@ -1,7 +1,6 @@
 <template>
 
-<p>On line persons</p>   
- 
+<p>On line persons</p>
 </template>
 
 <script>

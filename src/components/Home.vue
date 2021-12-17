d48e7f3c6baa662c187842483ba9087a1850da91
RobertaAb <robertaabreu77@gmail.com>
2021-12-16 22:27:35 -0300

    segunda aula de vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -30,13 +30,56 @@
       </v-menu>
     </v-app-bar>
     <v-main>
-      conteudo {{ nome }} {{sexo}} <br>
-      <input type="text" v-model="nome"/>
-      <br>
-      <input type="text" v-model="sexo"/>
+      conteudo {{ nome }} {{ sexo }} {{email}}{{ endereco }} {{ bairro }} {{ cidade }} {{ cep }} <br />
+      <br/>
+      <input type='text' v-model='nome' />
+      <br/>
+      <input type='text' v-model='sexo' />
+      <br/>
+      <input type='text' v-model='email' />
+      <br/>
+      <input type='text' v-model='endereco' />
+      <br/>
+      <input type='text' v-model='bairro' />
+      <br/>
+      <input type='text' v-model='cidade' />
+      <br/>
+      <input type='text' v-model='cep' />
+      <v-container>
+        <v-form>
+          <v-row>
+            <v-col cols='12' md='4'>
+              <v-text-field v-model='nome' label='Name' required></v-text-field>
+            </v-col>
+            <v-col cols='12' md='4'>
+              <v-text-field v-model='sexo' label='Sexo' required></v-text-field>
+            </v-col>
+            <v-col cols='12' md='4'>
+              <v-text-field v-model='email' label='E-mail' required></v-text-field>
+            </v-col>
+            <v-col cols='12' md='4'>
+              <v-text-field v-model='endereco' label='Endereço (rua / avenida / numero)' required></v-text-field>
+            </v-col>
+            <v-col cols='12' md='4'>
+              <v-text-field v-model='bairro' label='Bairro' required></v-text-field>
+            </v-col>
+            <v-col cols='12' md='4'>
+              <v-text-field v-model='cidade' label='Cidade' required></v-text-field>
+            </v-col><v-col cols='12' md='4'>
+              <v-text-field v-model='cep' label='CEP' required></v-text-field>
+            </v-col>
+            <v-col>
+              <v-btn
+                elevation="2"
+                outlined @click="minhaFuncao"
+              >BOTÃO</v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-container>
     </v-main>
-    <v-footer v:bind="fixed" padless>
-      <v-col class="text-center" cols="12">
+    <v-footer v:bind='fixed' padless>
+      <v-col class='text-center' cols='12'>
         {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
       </v-col>
     </v-footer>
@@ -48,12 +91,19 @@ export default {
   data () {
     return {
       nome: 'Alice',
-      sexo: 'Feminino'
+      sexo: 'Feminino',
+      email: 'roberta77@gmail.com',
+      endereco: 'Av. A, num:102, casa 7',
+      bairro: 'Bosque do Caju',
+      cidade: 'Araraquara',
+      cep: '13569-254'
     }
   },
   methods: {
     minhaFuncao () {
-      console.log('minhaFuncao')
+      // const msn = 'O nome da pessoa é : ' + this.nome + ' e o sexo é : ' + this.sexo
+      const msg = `O nome é : ${this.nome} ; \n sexo : ${this.sexo} ; \n email : ${this.email} ;\n endereço: ${this.endereco} ; \n bairro : ${this.bairro} ; \n cidade : ${this.cidade} ; \n CEP : ${this.cep} .`
+      alert(msg)
     }
   }
 }

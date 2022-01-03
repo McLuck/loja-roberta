456aaa0440120d56660a1474f510729ded41c9a2
RobertaAb <robertaabreu77@gmail.com>
2022-01-03 20:56:47 -0300

    reescrever form

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
+      <v-select :items= "items"></v-select>
+      {{pessoas}}
+      pessoas
+      <table >
+        <tr>
+          <td>
+            nome
+          </td>
+          <td>
+            email
+          </td>
+        </tr>
+         <tr v-for= "variavel in pessoas" :key= "variavel.id">
+          <td>
+            {{variavel.nome}}
+          </td>
+          <td>
+            {{variavel.email}}
+          </td>
+        </tr>
+      </table>
     <v-app-bar color="deep-purple accent-4" dense dark>
       <v-app-bar-nav-icon>
         <v-btn icon>
@@ -17,59 +38,88 @@
             <v-icon>mdi-account-circle</v-icon>
           </v-btn>
         </template>
+         </v-menu>
+      </v-app-bar>
+      <v-main>
+        <v-container>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="20"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
 
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
-    <v-main>
-      conteudo {{ nome }} {{ sexo }} {{email}}{{ endereco }} {{ bairro }} {{ cidade }} {{ cep }} <br />
-      <v-container>
-        <form @submit = "checkForm,checkCep, checkFilhos ,checkNome" action="https://vuejs.org/" method="post">
-        <p v-if = "errors.length">
-          <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
-          <ul>
-            <li v-for= "error in errors" v-bind:key= "error">{{ error }}</li>
-          </ul>
-          </p>
-          <v-row>
-            <v-col cols="12" md="4">
-              <v-text-field :rules = "regraNome" v-model="nome" label="Nome" ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field type="number" v-model="filhos" label="Filhos" ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field v-model="sexo" label="Sexo" ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field :rules= "regraEmail" v-model="email" label="E-mail" ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field v-model="endereco" label="Endereço (rua / avenida / numero)" ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field v-model="bairro" label="Bairro" ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field v-model="cidade" label="Cidade" ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field v-model="cep" label="CEP" ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-btn
-                elevation="2"
-                outlined type= "submit" >BOTÃO</v-btn>
-            </v-col>
-          </v-row>
-        </form>
-      </v-container>
-    </v-main>
-    <v-footer v:bind="fixed" padless>
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="endereco.logradouro"
+      :counter="20"
+      :rules="endereco.enderecoRules"
+      label="endereco"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="endereco.bairro"
+      :counter="20"
+      :rules="endereco.bairroRules"
+      label="Bairro"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="endereco.cidade"
+      :counter="20"
+      :rules="endereco.cidadeRules"
+      label="Cidade"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="endereco.estado"
+      :counter="10"
+      :rules="endereco.estadoRules"
+      label="Estado"
+      required
+    ></v-text-field>
+    <v-btn
+      :disabled="!valid"
+      color= black,
+      class="mr-4"
+      @click="validate"
+    >
+      Salvar
+    </v-btn>
+
+    <v-btn
+      color= black,
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+    <v-btn
+      color= red,
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn>
+  </v-form>
+  </v-container>
+</v-main>
+   <v-footer v:bind="fixed" padless>
       <v-col class="text-center" cols="12">
         {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
       </v-col>
@@ -79,109 +129,79 @@
 
 <script>
 export default {
-  el: '#Home',
-  data () {
-    return {
-      pessoa :{
-        nome: null,
-        filhos: null,
-        sexo: null,
-        email: null,
-        endereco: null,
-        bairro: null,
-        cidade: null,
-        cep: null
-      },
-      errors: [],
-      regraEmail: [
-        value => !!value || 'Required.',
-        value => (value || '').length <= 20 || 'Max 20 characters',
-        value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
-        }
+  data: () => ({
+    pessoas: [],
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+    ],
+    select: null,
+    items: [
+      '0 filhos',
+      '1 filhos',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10'
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ],
+    endereco: {
+      logradouro: null,
+      enderecoRules: [
+        v => !!v || 'Enderecço is required',
+        v => (v && v.length <= 10) || 'Endereço must be valid'
+      ],
+      bairroRules: [
+        v => !!v || 'Bairro is required',
+        v => (v && v.length <= 10) || 'Bairro must be valid'
+      ],
+      cidadeRules: [
+        v => !!v || 'Cidade is required',
+        v => (v && v.length <= 10) || 'Cidade must be valid'
+      ],
+      estadoRules: [
+        v => !!v || 'Estado is required',
+        v => (v && v.length <= 10) || 'Estado must be valid'
       ]
     }
-  },
+  }),
+
   methods: {
-    minhaFuncao () {
-      // const msn = 'O nome da pessoa é : ' + this.nome + ' e o sexo é : ' + this.sexo
-      const msg = `O nome é : ${this.nome}  \n e qtde de filhos(a) :  ${this.filhos} \n sexo : ${this.sexo}  \n email : ${this.email} \n endereço: ${this.endereco} \n bairro : ${this.bairro} \n cidade : ${this.cidade} \n CEP : ${this.cep} `
-      alert(msg)
-    },
-    checkTudo: function(){
-      checkForm = { function (e) {
-      if (this.nome && this.filhos && this.sexo && this.email && this.endereco && this.bairro && this.cidade && this.cep) {
-        return true
-      }
-      this.errors = []
-      if (!this.nome) {
-        this.errors.push('o nome é obrigatório')
-      }
-      if (!this.filhos) {
-        this.errors.push('quantidade de filhos é obrigatório')
-      }
-      if (!this.sexo) {
-        this.errors.push('o sexo é obrigatório')
-      }
-      if (!this.email) {
-        this.errors.push('o e-mail é obrigatório')
-      }
-      if (!this.endereco) {
-        this.errors.push('o endereço é obrigatório')
-      }
-      if (!this.bairro) {
-        this.errors.push('o bairro é obrigatório')
-      }
-      if (!this.cidade) {
-        this.errors.push('a cidade é obrigatório')
-      }
-      if (!this.cep) {
-        this.errors.push('o cep é obrigatório')
-      }
-      e.preventDefault()
-    },
-    
-
-   checkCep = { function (strCEP, blnVazio) {
-            var objER = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/
-             strCEP = Trim(strCEP)
-            if (strCEP.length > 0) {
-              if(objER.test(strCEP))
-                        return true
-                    else{
-                        return false
-                        }
-                    }else {
-          return blnVazio;
-        }
-            }
-  },
-
-    checkFilho = { function () {
-      var number = filhos.toFixed(2)
-      return number;
-    }
-    },
-
-    checkSexo = { function (){
-      var sexo = [feminino, masculino]
-      if (sexo == [0] || sexo [1]) {
-        return true
-      }else{
-        console.log("feminino ou masculino")
-      }
-    },
-    checkName = { function (input, inputName){
-      let inputToCompare = document.getElementsByName(inputName)[0]
-      let errorMessage = `Este campo precisa estar igual ao ${inputName}`
-          if (input.value !== inputToCompare.value) {
-            this.printMessage(input, errorMessage)
+    validate () {
+      if (this.$refs.form.validate()) {
+        this.pessoas.push({
+          id: new Date(),
+          nome: this.name,
+          email: this.email,
+          endereco: {
+            logradouro: this.endereco.logradouro
           }
+        })
       }
+    },
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
     }
   }
+}
 </script>
 
 <style>
+table,tr,td {
+  border: solid
+}
 </style>
